1c99ad47361cece51b4e5e3d78435f442e593840
Johns Gaming PC <1998johngtz@gmail.com>
2023-07-21 17:38:21 -0500

    some javascript done

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var APIKey = '6720568af4c60ae883e524b716027e2c';
+
+function getWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
+      .then(response => response.json())
+      .then(data => {
+          document.getElementById('currentWeather').innerHTML = `
+          <div class="card bg-primary text-white m-2" style="width: 18rem;">
+              <div class="card-body">
+                  <h5 class="card-title">${data.name} (${new Date().toLocaleDateString()})</h5>
+                  <p class="card-text">Temperature: ${data.main.temp} F</p>
+                  <p class="card-text">Wind Speed: ${data.wind.speed} MPH</p>
+                  <p class="card-text">Humidity: ${data.main.humidity} %</p>
+              </div>
+          </div>
+      `;
+      })
